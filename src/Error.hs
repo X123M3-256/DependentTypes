6d18ebe38bfa8f1d1f3d2e1a433e3104b8c395dc
X123M3-256 <edward@knaphill.me.uk>
2025-08-24 17:29:42 +0100

    Fix types not being simplified in error messages

--- src/Error.hs
+++ src/Error.hs
@@ -14,9 +14,9 @@ printError (ty,ctx,sr)=
 			case ty of
 				UndeclaredIdentifier name 	-> "Undeclared identifier \""++name++"\""
 				DuplicateBinding name		-> "Redefinition of \""++name++"\""
-				ExpectedPiType expr 		-> "Expected function type but recieved:\n\n\t\t"++(printExpr ctx expr)++"\n\n\t" --TODO better phrasing
-				ExpectedSigmaType expr 		-> "Expected pair type but recieved:\n\n\t\t"++(printExpr ctx expr)++"\n\n\t"
-				ExpectedUniverse expr ty	-> "Expected universe but recieved:\n\n\t\t"++(printExpr ctx ty)++"\n\n\t in expression\n\n\t\t"++(printExpr ctx expr)++"\n\n\t"
+				ExpectedPiType expr 		-> "Expected function type but recieved:\n\n\t\t"++(printExpr ctx (simplify ctx expr))++"\n\n\t" --TODO better phrasing
+				ExpectedSigmaType expr 		-> "Expected pair type but recieved:\n\n\t\t"++(printExpr ctx (simplify ctx expr))++"\n\n\t"
+				ExpectedUniverse expr ty	-> "Expected universe but recieved:\n\n\t\t"++(printExpr ctx (simplify ctx ty))++"\n\n\t in expression\n\n\t\t"++(printExpr ctx expr)++"\n\n\t"
 				TypeMismatch expr expect given	-> "Type mismatch - expected:\n\n\t\t"++(printExpr ctx (simplify ctx expect))++"\n\n\tbut recieved\n\n\t\t"++(printExpr ctx (simplify ctx given))++"\n\n\tin expression \""++(printExpr ctx expr)++"\""
 		in
 			"Error:\n\t"++msg++" at line "++(show line)++", column "++(show col)
